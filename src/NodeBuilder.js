d402ca3823da4517232a01ea2fef55fc13194d65
Amir Valizadeh <avalizadeh@cribl.io>
2025-04-15 18:16:20 +0200

    Enable configure.py patch
    - Implement the logic from https://github.com/avcribl/node/commit/2a185d128825d2b875da9d875d26fcf276ef04b4
    
    Co-authored-by: Michal Biesek <mbiesek@cribl.io>

--- src/NodeBuilder.js
+++ src/NodeBuilder.js
@@ -214,9 +214,9 @@ class NodeJsBuilder {
       this.nodePath('node.gyp'),
       join(this.patchDir, 'node.gyp.patch'));
 
-    // await patchFile( 
-    //   this.nodePath('configure.py'),
-    //   join(this.patchDir, 'configure.py.patch'));
+    await patchFile(
+      this.nodePath('configure.py'),
+      join(this.patchDir, 'configure.py.patch'));
 
     if (isWindows) {
       await patchFile(this.nodePath('vcbuild.bat'), join(this.patchDir, 'vcbuild.bat.patch'));
